a284743a659a986a6d6edf09237635cd4bcb3d00
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-11 18:09:39 +0200

    [UPD] Created form

--- index.php
+++ index.php
@@ -15,7 +15,23 @@
         </div>
         <div class="row mt-4">
             <div class="col-12">
-                
+            <form action="./index.php" method="GET">
+                    <div class="row">
+                        <div class="col-5">
+                            <select name="parking" id="parking" class="form-select form-select-sm">
+                                <option value="">Parcheggio</option>
+                                <option value="0">NO</option>
+                                <option value="1">SI</option>
+                            </select>
+                        </div>
+                        <div class="col-5">
+                            <input type="text" class="form-control form-control-sm" name="vote" placeholder="voto">
+                        </div>
+                        <div class="col-2">
+                            <button type="submit" class="btn btn-sm btn-primary">Cerca</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>

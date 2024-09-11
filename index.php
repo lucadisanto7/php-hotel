609c31c8c98c4e25fc06de3bc30df5b15dee0d6c
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-11 18:20:23 +0200

    [UPD] Added table

--- index.php
+++ index.php
@@ -58,6 +58,7 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
 <body>
@@ -88,6 +89,26 @@ $hotels = [
                 </form>
             </div>
         </div>
+        <table class="table table-striped table-dark">
+            <thead>
+                <?php foreach ($hotels[0] as $key => $value) { ?>
+                    <th class="text-center">        
+                        <?php echo ucfirst(str_replace('_', ' ', $key))?>                        
+                    </th>
+                <?php } ?>
+            </thead>
+            <tbody>
+                <?php foreach( $hotels as $hotel) {?>
+                    <tr>
+                        <td class="text-center"><?php echo$hotel['name']?></td>
+                        <td class="text-center"><?php echo$hotel['description']?></td>
+                        <td class="text-center"><?php echo$hotel['parking'] ? 'yes' : 'no' ?></td>
+                        <td class="text-center"><?php echo$hotel['vote']?></td>
+                        <td class="text-center"><?php echo$hotel['distance_to_center']?>km</td>
+                    </tr>
+                <?php }?>
+            </tbody>
+        </table>
     </div>
 </body>
 </html>
